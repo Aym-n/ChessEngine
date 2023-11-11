d70ba06006306d0e38e25ad71b1377eb614deb63
aym-n <ajm8982@gmail.com>
2023-11-11 07:53:40 +0530

    Added textures and minor refractoring

--- src/chess.rs
+++ src/chess.rs
@@ -3,14 +3,24 @@ extern crate sdl2;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use sdl2::image::{InitFlag, LoadTexture};
+use sdl2::render::Texture;
 
-
-#[derive(Copy , Clone)]
-struct Piece {
-    ptype: PieceType,
+#[derive(Copy, Clone)]
+pub struct Piece {
+    p_type: PieceType,
     p_color: PColor,
 }
- 
+
+impl Piece {
+    fn new_white(p_type: PieceType) -> Piece {
+        Piece { p_type: (p_type), p_color: (PColor::White)}
+    }
+    
+    fn new_black(p_type: PieceType) -> Piece {
+        Piece { p_type: (p_type), p_color: (PColor::Black)}
+    }
+}
 #[derive(Copy , Clone, Debug)]
 enum PieceType{
     Pawn,
@@ -21,7 +31,7 @@ enum PieceType{
     King,
 }
 
-#[derive(Copy , Clone, PartialEq)]
+#[derive(Copy , Clone, PartialEq, Debug)]
 enum PColor{
     White,
     Black,
@@ -39,7 +49,7 @@ fn print_board(board: &Board) {
         for file in 0..8 {
             match board.squares[rank][file] {
                 Some(piece) => {
-                    let piece_symbol = match piece.ptype {
+                    let piece_symbol = match piece.p_type {
                         PieceType::Pawn => "♙",
                         PieceType::Rook => "♖",
                         PieceType::Knight => "♘",
@@ -68,7 +78,7 @@ fn move_generation(board: &Board) -> Vec<(usize, usize)>{
     match board.selected_piece {
         Some((rank, file)) => {
             let piece = board.squares[rank][file].unwrap();
-            match piece.ptype {
+            match piece.p_type {
                 PieceType::Pawn => {
                     // Generate pawn moves
                     let direction = match piece.p_color {
@@ -227,111 +237,63 @@ impl Board{
             selected_piece: None,
             turn: PColor::White,
         };
-    
-        // Set up the white pieces (top of the board)
-        board.squares[0][0] = Some(Piece {
-            ptype: PieceType::Rook,
-            p_color: PColor::White,
-        });
-        board.squares[0][1] = Some(Piece {
-            ptype: PieceType::Knight,
-            p_color: PColor::White,
-        });
-        board.squares[0][2] = Some(Piece {
-            ptype: PieceType::Bishop,
-            p_color: PColor::White,
-        });
-        board.squares[0][3] = Some(Piece {
-            ptype: PieceType::Queen,
-            p_color: PColor::White,
-        });
-        board.squares[0][4] = Some(Piece {
-            ptype: PieceType::King,
-            p_color: PColor::White,
-        });
-        board.squares[0][5] = Some(Piece {
-            ptype: PieceType::Bishop,
-            p_color: PColor::White,
-        });
-        board.squares[0][6] = Some(Piece {
-            ptype: PieceType::Knight,
-            p_color: PColor::White,
-        });
-        board.squares[0][7] = Some(Piece {
-            ptype: PieceType::Rook,
-            p_color: PColor::White,
-        });
-    
-        // Set up the black pieces (bottom of the board)
-        board.squares[7][0] = Some(Piece {
-            ptype: PieceType::Rook,
-            p_color: PColor::Black,
-        });
-        board.squares[7][1] = Some(Piece {
-            ptype: PieceType::Knight,
-            p_color: PColor::Black,
-        });
-        board.squares[7][2] = Some(Piece {
-            ptype: PieceType::Bishop,
-            p_color: PColor::Black,
-        });
-        board.squares[7][3] = Some(Piece {
-            ptype: PieceType::Queen,
-            p_color: PColor::Black,
-        });
-        board.squares[7][4] = Some(Piece {
-            ptype: PieceType::King,
-            p_color: PColor::Black,
-        });
-        board.squares[7][5] = Some(Piece {
-            ptype: PieceType::Bishop,
-            p_color: PColor::Black,
-        });
-        board.squares[7][6] = Some(Piece {
-            ptype: PieceType::Knight,
-            p_color: PColor::Black,
-        });
 
-        board.squares[7][7] = Some(Piece {
-            ptype: PieceType::Rook,
-            p_color: PColor::Black,
-        });
-    
-        for i in 0..8 {
-            board.squares[6][i] = Some(Piece {
-                ptype: PieceType::Pawn,
-                p_color: PColor::Black,
-            });
+        //Starting position
+        //Set up white pieces
 
-            board.squares[1][i] = Some(Piece {
-                ptype: PieceType::Pawn,
-                p_color: PColor::White,
-            });
-
+        for file in 0..8{
+            board.new_piece(Piece::new_white(PieceType::Pawn), 1, file);
         }
+        board.new_piece(Piece::new_white(PieceType::Rook), 0, 0);
+        board.new_piece(Piece::new_white(PieceType::Rook), 0, 7);
+        board.new_piece(Piece::new_white(PieceType::Knight), 0, 1);
+        board.new_piece(Piece::new_white(PieceType::Knight), 0, 6);
+        board.new_piece(Piece::new_white(PieceType::Bishop), 0, 2);
+        board.new_piece(Piece::new_white(PieceType::Bishop), 0, 5);
+        board.new_piece(Piece::new_white(PieceType::Queen), 0, 3);
+        board.new_piece(Piece::new_white(PieceType::King), 0, 4);
+        
+        //Set up black pieces
+        for file in 0..8{
+            board.new_piece(Piece::new_black(PieceType::Pawn), 6, file);
+        }
+        board.new_piece(Piece::new_black(PieceType::Rook), 7, 0);
+        board.new_piece(Piece::new_black(PieceType::Rook), 7, 7);
+        board.new_piece(Piece::new_black(PieceType::Knight), 7, 1);
+        board.new_piece(Piece::new_black(PieceType::Knight), 7, 6);
+        board.new_piece(Piece::new_black(PieceType::Bishop), 7, 2);
+        board.new_piece(Piece::new_black(PieceType::Bishop), 7, 5);
+        board.new_piece(Piece::new_black(PieceType::Queen), 7, 3);
+        board.new_piece(Piece::new_black(PieceType::King), 7, 4);
+
+
+  
         board
     }
 
     pub fn display(&mut self){
 
-        const SQUARE_SIZE: u32 = 100;
+        const WINDOW_SIZE: u32 = 1000;
+        const SQUARE_SIZE: u32 = WINDOW_SIZE / BOARD_SIZE;
         const BOARD_SIZE: u32 = 8;
+        const PIECE_SIZE: u32 = SQUARE_SIZE *  9 / 10 ;
 
         let mut _moves: Vec<(usize, usize)> = Vec::new();
 
         let sdl_context = sdl2::init().unwrap();
         let video_subsystem = sdl_context.video().unwrap();
-    
+        let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG)
+        .expect("Couldn't initialize image context");
         let window = video_subsystem
-            .window("Chess", 800, 800)
+            .window("Chess", WINDOW_SIZE, WINDOW_SIZE)
             .position_centered()
             .build()
             .unwrap();
     
         let mut canvas = window.into_canvas().build().unwrap();
-    
+
         canvas.set_draw_color(Color::RGB(255, 255, 255));
-        canvas.clear();
+        // canvas.clear();
         canvas.present();
     
         let mut event_pump = sdl_context.event_pump().unwrap();
@@ -347,8 +309,8 @@ impl Board{
                     Event::MouseButtonDown {x, y, .. } => {
                         
                         // Get the rank and file of the square that was clicked
-                        let file = x / 100;
-                        let rank = 7 - (y / 100);
+                        let file = x as u32 / SQUARE_SIZE;
+                        let rank = 7 - (y as u32 / SQUARE_SIZE);
 
 
 
@@ -429,28 +391,30 @@ impl Board{
                     canvas.fill_rect(square).unwrap();
 
                     if let Some(piece) = self.squares[rank as usize][file as usize] {
-                        let piece_color = match piece.p_color {
-                            PColor::White => sdl2::pixels::Color::RGB(255, 255, 255), // White
-                            PColor::Black => sdl2::pixels::Color::RGB(0, 0, 0), // Black
-                        };
-            
-                        // Using Squares to represent pieces 
+
                         let piece_rect = sdl2::rect::Rect::new(
-                            (x + SQUARE_SIZE / 4) as i32,
-                            (y + SQUARE_SIZE / 4) as i32,
-                            SQUARE_SIZE / 2,
-                            SQUARE_SIZE / 2,
+                            (x + (SQUARE_SIZE - PIECE_SIZE) / 2) as i32,
+                            (y + (SQUARE_SIZE - PIECE_SIZE) / 2) as i32,
+                            PIECE_SIZE,
+                            PIECE_SIZE,
                         );
             
-                        canvas.set_draw_color(piece_color);
-                        canvas.fill_rect(piece_rect).unwrap();
+                        let texture_string = format!("textures/{}-{}.svg",format!("{:?}", piece.p_type).split("::").last().unwrap().to_lowercase(),format!("{:?}", piece.p_color).split("::").last().unwrap().to_lowercase());
+                        let texture_creator = canvas.texture_creator();
+                        let texture = texture_creator.load_texture(texture_string).unwrap();
+                    
+                        canvas.copy(&texture, None, piece_rect)
+                        .expect("Failed to copy image.");
                     }
                 }
             }
-    
+            
             canvas.present();
         }
     }
 
+    pub fn new_piece(&mut self, piece: Piece, rank: usize, file: usize){
+        self.squares[rank][file] = Some(piece);
+    }
 }
                    
